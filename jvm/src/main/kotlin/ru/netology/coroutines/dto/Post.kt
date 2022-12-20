44fd4f5276d1142e731a014285911d9e7b239c35
aleksandrgrigorev <xundr1992@gmail.com>
2022-12-20 18:42:23 +0400

    Redundant comments removed

--- jvm/src/main/kotlin/ru/netology/coroutines/dto/Post.kt
+++ jvm/src/main/kotlin/ru/netology/coroutines/dto/Post.kt
@@ -1,14 +1,6 @@
 package ru.netology.coroutines.dto
 
 data class Post(
-//    val id: Long,
-//    val author: String,
-//    val authorAvatar: String,
-//    val content: String,
-//    val published: Long,
-//    val likedByMe: Boolean,
-//    val likes: Int = 0,
-
     val id: Long,
     val authorId: Long,
     val content: String,
